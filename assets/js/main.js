cffb275e73c7cf6a21bef2c2ec5f82330d05761c
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-23 21:51:10 +0100

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let headline = document.querySelector("h1")
+let background = document.querySelector("body")
+
+let colorChange = (tc,bgc) =>{
+    headline.style.color = tc;
+    background.style.backgroundColor = bgc;
+}
